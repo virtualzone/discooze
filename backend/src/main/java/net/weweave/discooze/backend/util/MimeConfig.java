0568ca16660859c0c4ed81a949d9df087f6fa913
Heiner <heiner.peuser@gmail.com>
2018-01-10 19:19:15 +0100

    Added Mime Config

---
+++ backend/src/main/java/net/weweave/discooze/backend/util/MimeConfig.java
@@ -0,0 +1,16 @@
+package net.weweave.discooze.backend.util;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MimeMappings;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MimeConfig implements EmbeddedServletContainerCustomizer {
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        mappings.add("js", "application/javascript; charset=utf-8");
+        container.setMimeMappings(mappings);
+    }
+}
