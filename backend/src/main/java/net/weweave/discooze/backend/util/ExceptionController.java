556ed8674478db3c79fcf7f4708eb0bf1440129a
Heiner <heiner.peuser@gmail.com>
2018-01-15 22:03:09 +0100

    Added content-type to html5 path handler

--- backend/src/main/java/net/weweave/discooze/backend/util/ExceptionController.java
+++ backend/src/main/java/net/weweave/discooze/backend/util/ExceptionController.java
@@ -1,12 +1,15 @@
 package net.weweave.discooze.backend.util;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -20,7 +23,7 @@ public class ExceptionController {
     private String publicBasePath;
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<String> notFound(HttpServletRequest request) throws IOException {
+    public ResponseEntity<String> notFound(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String requestUri = request.getRequestURI().substring(request.getContextPath().length());
         if (!requestUri.startsWith("/api/")) {
             System.out.println("Not found: " + requestUri + " --> replacing with index");
@@ -29,7 +32,10 @@ public class ExceptionController {
             byte[] fileBytes = Files.readAllBytes(path);
             String content = new String(fileBytes);
             content = content.replace("[PUBLIC_BASE_PATH]", this.publicBasePath);
-            return ResponseEntity.ok(content);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE + "; charset=utf-8");
+            return new ResponseEntity<>(content, headers, HttpStatus.OK);
         } else {
             return ResponseEntity.notFound().build();
         }
